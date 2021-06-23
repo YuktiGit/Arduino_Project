8795b5d398e6f881c70dd04682d02424a729d894
YuktiGit <86337915+YuktiGit@users.noreply.github.com>
2021-06-23 10:27:47 +0530

    Update Final_Project.ino

--- Final_Project/Final_Project.ino
+++ Final_Project/Final_Project.ino
@@ -1,1 +1,92 @@
+int ir1;
+int ir2;
+int ir3;
+int ir4;
 
+int led1;
+int led2;
+int led3;
+int led4;
+int led5;
+int led6;
+
+
+void setup() {
+  int sen1 = LOW;
+  int sen2 = LOW;
+  int sen3 = LOW;
+  int sen4 = LOW;
+
+  pinMode(ir1, INPUT);
+  pinMode(ir2, INPUT);
+  pinMode(ir3, INPUT);
+  pinMode(ir4, INPUT);
+
+  pinMode(led1, OUTPUT);
+  pinMode(led2, OUTPUT);
+  pinMode(led3, OUTPUT);
+  pinMode(led4, OUTPUT);
+  pinMode(led5, OUTPUT);
+  pinMode(led6, OUTPUT);
+}
+
+
+
+void loop() {
+  sen1 = digitalRead(ir1);
+  sen2 = digitalRead(ir2);
+  sen3 = digitalRead(ir3);
+  sen4 = digitalRead(ir4);
+
+  if(sen1 == HIGH) {
+    digitalWrite(led1, HIGH);
+    digitalWrite(led2, HIGH);
+    digitalWrite(led3, HIGH);
+  }
+
+  else {
+    digitalWrite(led1, LOW);
+    digitalWrite(led2, LOW);
+    digitalWrite(led3, LOW);
+  }
+
+  if(sen2 == HIGH) {
+    digitalWrite(led2, HIGH);
+    digitalWrite(led3, HIGH);
+    digitalWrite(led4, HIGH);
+  }
+
+  else {
+    digitalWrite(led2, LOW);
+    digitalWrite(led3, LOW);
+    digitalWrite(led4, LOW);
+  }
+
+
+  if(sen3 == HIGH) {
+    digitalWrite(led3, HIGH);
+    digitalWrite(led4, HIGH);
+    digitalWrite(led5, HIGH);
+  }
+
+  else {
+    digitalWrite(led3, LOW);
+    digitalWrite(led4, LOW);
+    digitalWrite(led5, LOW);
+  }
+
+
+
+  if(sen4 == HIGH) {
+    digitalWrite(led4, HIGH);
+    digitalWrite(led5, HIGH);
+    digitalWrite(led6, HIGH);
+  }
+
+  else {
+    digitalWrite(led4, LOW);
+    digitalWrite(led5, LOW);
+    digitalWrite(led6, LOW);
+  }
+
+}
